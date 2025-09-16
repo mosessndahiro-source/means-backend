6d0af0bf19cb6f11b170cfa117d2d83adf12845f
mosessndahiro-source <mosessndahiro@gmail.com>
2025-09-16 10:09:15 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\AuthController;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
 
 /*
 |--------------------------------------------------------------------------
