d0f087e40b83c4814aa45e6f6f67a37d0b22898a
mosessndahiro-source <mosessndahiro@gmail.com>
2025-12-19 06:41:30 +0200

    Add custom CORS middleware to global and api groups

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -36,7 +36,6 @@ protected $middleware = [
     \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
     \App\Http\Middleware\TrimStrings::class,
     \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-];
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
