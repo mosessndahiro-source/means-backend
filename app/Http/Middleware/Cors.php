95db4fc3bcce3b98798ecb990be09045ba6ac6c0
mosessndahiro-source <mosessndahiro@gmail.com>
2025-12-19 06:10:40 +0200

    Fix CORS with custom middleware that handles OPTIONS preflight

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -8,16 +8,19 @@ class Cors
 {
     public function handle($request, Closure $next)
     {
+        // Handle preflight OPTIONS request
+        if ($request->getMethod() === "OPTIONS") {
+            return response('', 200)
+                ->header('Access-Control-Allow-Origin', '*')
+                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+                ->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization, X-Request-With, X-XSRF-TOKEN');
+        }
+
         $response = $next($request);
 
         $response->headers->set('Access-Control-Allow-Origin', '*');
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         $response->headers->set('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization, X-Request-With, X-XSRF-TOKEN');
-
-        // Handle preflight OPTIONS request
-        if ($request->getMethod() === "OPTIONS") {
-            $response->setStatusCode(200);
-        }
 
         return $response;
     }
