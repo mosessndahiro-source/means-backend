2b170f6d19c0985678955580066982e7f6bd1cd5
mosessndahiro-source <mosessndahiro@gmail.com>
2025-12-19 09:04:32 +0200

    Ultimate CORS fix: custom middleware with explicit OPTIONS handling

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,13 +14,12 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
-\Fruitcake\Cors\HandleCors::class,
+\App\Http\Middleware\Cors::class,  // <-- MUST be here (early)
     \App\Http\Middleware\TrustProxies::class,
     \App\Http\Middleware\CheckForMaintenanceMode::class,
     \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
     \App\Http\Middleware\TrimStrings::class,
-    \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-    ];
+    \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,    ];
 
     /**
      * The application's route middleware groups.
