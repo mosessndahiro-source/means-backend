fc1272d0dd90f6d987301d6e53392f84d3572ec8
mosessndahiro-source <mosessndahiro@gmail.com>
2025-12-23 19:06:58 +0200

    Fix CORS by using Fruitcake global middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -10,7 +10,7 @@ class Kernel extends HttpKernel
      * The application's global HTTP middleware stack.
      */
     protected $middleware = [
-        \Fruitcake\Cors\HandleCors::class, // ✅ ONLY CORS HANDLER
+       
         \App\Http\Middleware\TrustProxies::class,
         \App\Http\Middleware\CheckForMaintenanceMode::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
