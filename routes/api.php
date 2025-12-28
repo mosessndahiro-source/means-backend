403a9b92a908d5414251085177dbe55a23516724
mosessndahiro-source <mosessndahiro@gmail.com>
2025-12-28 06:03:49 +0200

    Your message here

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,6 @@
 <?php
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
-
-// Authentication Routes
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/check-user', [AuthController::class, 'checkUser']);
-
 
 /*
 |--------------------------------------------------------------------------
